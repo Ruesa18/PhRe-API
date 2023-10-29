4e68a318b7710c65b8eae44738f9f4b71222f66c
Sandro Rüfenacht <sandro.ruefenacht0@gmail.com>
2023-10-29 12:51:22 +0100

    feat: add router

---
+++ src/kernel/utils/Router.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace PHREAPI\kernel\utils;
+
+use PHREAPI\api\endpoints\EndpointInterface;
+use PHREAPI\api\Routes;
+use PHREAPI\kernel\utils\enums\HttpMethod;
+use PHREAPI\kernel\utils\input\Request;
+use PHREAPI\kernel\utils\output\JSONResponse;
+use PHREAPI\kernel\utils\output\ResponseInterface;
+
+class Router {
+    public function __construct(protected Routes $routes = new Routes()) {}
+
+    public function handleRequestedUrl($url): ResponseInterface {
+        $pos = strpos($url, '/', 1);
+        if($pos !== false) {
+            $urlParts = str_split($url, strpos($url, '/', 1));
+        } else {
+            $urlParts = [$url];
+        }
+
+        $endpoint = $this->routes->getEndpoint($urlParts[0]);
+
+        if(!$endpoint) {
+            return new JSONResponse(404);
+        }
+
+        $instance = new $endpoint();
+        $request = new Request();
+        $url = '' . $urlParts[0];
+        array_shift($urlParts);
+
+        return $this->callByHttpMethod($instance, $request->setUrl($url)->setParameters($urlParts));
+    }
+
+    public function callByHttpMethod(?EndpointInterface $endpoint, Request $request): ResponseInterface {
+        if($endpoint !== null) {
+            switch($request->getMethod()) {
+                case HttpMethod::GET:
+                    return $endpoint->index($request);
+                case HttpMethod::POST:
+                    return $endpoint->create($request);
+                case HttpMethod::DELETE:
+                    return $endpoint->delete($request);
+            }
+        }
+        return new JSONResponse(404, 'Not Found');
+    }
+}
